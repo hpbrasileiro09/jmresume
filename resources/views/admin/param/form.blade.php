f251af258c4bef72dab56090b16723cf77660072
Hernando Brasileiro <hpbrasileiro@hotmail.com>
2018-09-03 10:03:22 -0300

    Little adjustment in form param

--- resources/views/admin/param/form.blade.php
+++ resources/views/admin/param/form.blade.php
@@ -25,45 +25,45 @@
 	
 	</div>
 </div>
-@endif
-
-@if ($register->type == "radio")
-<div class="form-group{{ $errors->has('value') ? ' has-error' : '' }}">
-	<label for="value" class="control-label col-sm-2">{{ $register->label or '' }}</label>
-	<div class="col-sm-10">
-		<div class="radio">
-		  @if ($register->value == "0")
-		  <label><input type="radio" name="value" value="0" checked>No</label>
- 	      @else
-		  <label><input type="radio" name="value" value="0">No</label>
- 	      @endif
-		</div>
-		<div class="radio">
-		  @if ($register->value == "1")
-		  <label><input type="radio" name="value" value="1" checked>Yes</label>
- 	      @else
-		  <label><input type="radio" name="value" value="1">Yes</label>
- 	      @endif
-		</div>
-	    @if ($errors->has('value'))
-	        <span class="help-block">
-	            <strong>{{ $errors->first('value') }}</strong>
-	        </span>
-	    @endif
-	</div>
-</div>	    
 @else
-<div class="form-group{{ $errors->has('value') ? ' has-error' : '' }}">
-	<label for="value" class="control-label col-sm-2">{{ $register->label or '' }}</label>
-	<div class="col-sm-10">
-		<input type="text" name="value" id="value" class="form-control" value="{{ $register->value or '0' }}" placeholder="{{ $register->label or '' }}">
-	    @if ($errors->has('value'))
-	        <span class="help-block">
-	            <strong>{{ $errors->first('value') }}</strong>
-	        </span>
-	    @endif
-	</div>
-</div>	    
+	@if ($register->type == "radio")
+	<div class="form-group{{ $errors->has('value') ? ' has-error' : '' }}">
+		<label for="value" class="control-label col-sm-2">{{ $register->label or '' }}</label>
+		<div class="col-sm-10">
+			<div class="radio">
+			  @if ($register->value == "0")
+			  <label><input type="radio" name="value" value="0" checked>No</label>
+	 	      @else
+			  <label><input type="radio" name="value" value="0">No</label>
+	 	      @endif
+			</div>
+			<div class="radio">
+			  @if ($register->value == "1")
+			  <label><input type="radio" name="value" value="1" checked>Yes</label>
+	 	      @else
+			  <label><input type="radio" name="value" value="1">Yes</label>
+	 	      @endif
+			</div>
+		    @if ($errors->has('value'))
+		        <span class="help-block">
+		            <strong>{{ $errors->first('value') }}</strong>
+		        </span>
+		    @endif
+		</div>
+	</div>	    
+	@else
+	<div class="form-group{{ $errors->has('value') ? ' has-error' : '' }}">
+		<label for="value" class="control-label col-sm-2">{{ $register->label or '' }}</label>
+		<div class="col-sm-10">
+			<input type="text" name="value" id="value" class="form-control" value="{{ $register->value or '0' }}" placeholder="{{ $register->label or '' }}">
+		    @if ($errors->has('value'))
+		        <span class="help-block">
+		            <strong>{{ $errors->first('value') }}</strong>
+		        </span>
+		    @endif
+		</div>
+	</div>	    
+	@endif
 @endif
 
 <div class="form-group">
